b3f7ce569702a91678c64973f6d0ddafc3859e3a
ubuntu-wutinale <wutinale@moruankeji.com>
2019-08-08 21:00:35 +0800

    修改

--- DCIM/app/src/main/java/com/wutianle/dcim/DelDcim.java
+++ DCIM/app/src/main/java/com/wutianle/dcim/DelDcim.java
@@ -2,6 +2,8 @@ package com.wutianle.dcim;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class DelDcim extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class DelDcim extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_del_dcim);
+
+        findViewById(R.id.buttondel).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
--- DCIM/app/src/main/java/com/wutianle/dcim/MainActivity.java
+++ DCIM/app/src/main/java/com/wutianle/dcim/MainActivity.java
@@ -1,26 +1,24 @@
 package com.wutianle.dcim;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
-import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
-
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnLongClickListener;
 import android.widget.Button;
+import android.support.v4.content.PermissionChecker;
+import android.provider.MediaStore;
+import android.content.Context;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
